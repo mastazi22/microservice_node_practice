f7ee314844327f8b6e678d6a2aa5523ccafd3826
shurjolaptop <mashure@shurjomukhi.com.bd>
2020-06-11 05:35:21 +0600

    added the post app

---
+++ blog/posts/index.js
@@ -0,0 +1,21 @@
+const express=require('expresss');
+const {radnomBytes}=require('crypto');
+const bodyParser=require('body-parser');
+const app=express;
+app.use(bodyParser.json());
+const posts={};
+app.get('/posts',(req,res)=>
+{
+    res.post(posts);
+});
+app.post('/posts',(req,res)=>{
+    const id=radnomBytes(4).toString('hex');
+    const{title}=req.body();
+    posts[id]={
+        id,title
+    };
+    res.status(201).send(postsp[id]);
+});
+app.listen(4000,()=>{
+console.log('listening on 4000');
+});
