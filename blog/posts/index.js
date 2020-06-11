5872df9b7a5fb12c1d4b12265d12355e56b1c658
shurjolaptop <mashure@shurjomukhi.com.bd>
2020-06-11 06:37:33 +0600

    express fix

--- blog/posts/index.js
+++ blog/posts/index.js
@@ -1,13 +1,16 @@
 const express=require('express');
 const {radnomBytes}=require('crypto');
 const bodyParser=require('body-parser');
+
 const app=express;
-app.use(bodyParser.json());
+
 const posts={};
+
 app.get('/posts',(req,res)=>
 {
     res.post(posts);
 });
+
 app.post('/posts',(req,res)=>{
     const id=radnomBytes(4).toString('hex');
     const{title}=req.body();
@@ -16,6 +19,7 @@ app.post('/posts',(req,res)=>{
     };
     res.status(201).send(postsp[id]);
 });
+
 app.listen(4000,()=>{
 console.log('listening on 4000');
 });
